ba7939ebcfb57f0f42dd84ca04214958ad126734
CHRISTOPHER A. MAUL <maulc@us.ibm.com>
2019-10-16 11:48:40 -0400

    Added release tagging

---
+++ .tag-release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [ -z $(git tag) ]
+then
+  echo "Using default tag"
+  TAG="v1.0.0"
+elif [ -z "$(git tag -l --points-at HEAD)" ]
+then
+  echo "git tags"
+  git tag
+  LATEST_VERISON=$(git tag| grep -o "v[0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*" | sort -Vr | head -n1)
+  echo "Latest Version: $LATEST_VERISON"
+  NEXT_VERSION=$(echo $LATEST_VERISON | cut -d '.' -f 1).$(echo $LATEST_VERISON | cut -d '.' -f 2).$(expr $( echo $LATEST_VERISON | cut -d '.' -f 3) + 1)
+  echo "NEXT_VERSION: $NEXT_VERSION"
+  TAG=$NEXT_VERSION
+else
+  echo "HEAD tags"
+  git tag -l --points-at HEAD
+  CURRENT_VERISON=$(git tag -l --points-at HEAD| grep -o "v[0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*" | sort -Vr | head -n1)
+  echo $CURRENT_VERISON
+  TAG="$CURRENT_VERISON-$(date +"%F-%H-%M")"
+fi
+git tag $TAG
+git push --tags
