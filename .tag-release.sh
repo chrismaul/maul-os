25ce4f4eb38fe0c28d7dded8f7ee31d395692ee6
CHRISTOPHER A. MAUL <maulc@us.ibm.com>
2019-10-16 15:00:53 -0400

    Changed the tag to not include the v

--- .tag-release.sh
+++ .tag-release.sh
@@ -2,12 +2,12 @@
 if [ -z $(git tag) ]
 then
   echo "Using default tag"
-  TAG="v1.0.0"
+  TAG="1.0.0"
 elif [ -z "$(git tag -l --points-at HEAD)" ]
 then
   echo "git tags"
   git tag
-  LATEST_VERISON=$(git tag| grep -o "v[0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*" | sort -Vr | head -n1)
+  LATEST_VERISON=$(git tag| grep -o "[0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*" | sort -Vr | head -n1)
   echo "Latest Version: $LATEST_VERISON"
   NEXT_VERSION=$(echo $LATEST_VERISON | cut -d '.' -f 1).$(echo $LATEST_VERISON | cut -d '.' -f 2).$(expr $( echo $LATEST_VERISON | cut -d '.' -f 3) + 1)
   echo "NEXT_VERSION: $NEXT_VERSION"
@@ -15,7 +15,7 @@ then
 else
   echo "HEAD tags"
   git tag -l --points-at HEAD
-  CURRENT_VERISON=$(git tag -l --points-at HEAD| grep -o "v[0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*" | sort -Vr | head -n1)
+  CURRENT_VERISON=$(git tag -l --points-at HEAD| grep -o "[0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*" | sort -Vr | head -n1)
   echo $CURRENT_VERISON
   TAG="$CURRENT_VERISON-$(date +"%F-%H-%M")"
 fi
