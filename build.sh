7792bdb5184c5db4355ddf50dfb8712ad483dfdd
CHRISTOPHER A. MAUL <maulc@us.ibm.com>
2019-10-16 09:21:19 -0400

    Updated the code to use mkosi

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+mkdir -p $PWD/mkosi.extra/packages
 docker pull archlinux/base
-docker run --rm --name build-arch \
-  -v $PWD:/code \
-  -it --privileged archlinux/base /code/install/build.sh
+docker run --rm  \
+  -e DESTDIR=/output \
+  -e SRCDIR=/build \
+  -v $PWD/build:/build \
+  -v $PWD/mkosi.extra/:/output \
+  -v $PWD/build-packages.sh:/run.sh \
+  -it archlinux/base /run.sh
