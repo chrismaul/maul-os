a160881f9649f627aa3d829ab5843390b12e3696
CHRISTOPHER A. MAUL <maulc@us.ibm.com>
2019-10-29 10:12:46 -0400

    Fixed bug

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ do
   docker build --target $TARGET -t $TARGET .
   docker run --name $TARGET -w / $TARGET mksquashfs usr boot /output.squashfs
 
-  test -e $OUTPUTDIR/$TARGET.squashfs || rm $OUTPUTDIR/$TARGET.squashfs
+  test -e $OUTPUTDIR/$TARGET.squashfs && rm $OUTPUTDIR/$TARGET.squashfs
   docker cp $TARGET:/output.squashfs $OUTPUTDIR/$TARGET.squashfs
   docker cp $TARGET:/boot $OUTPUTDIR/$TARGET
 
